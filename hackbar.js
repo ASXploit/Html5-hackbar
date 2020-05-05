5dab8481610ee9f9e756d08ffbc2d61aef236adc
Xploit <linkhto@Gmail.com>
2020-05-04 18:22:46 -0700

    Update hackbar.js

--- hackbar.js
+++ hackbar.js
@@ -1,9 +1,9 @@
 window.onload = function () {
     
 class Button {
-  constructor(Text, Key, Callback, Interval) {
+  constructor(Text, Key, Callback, Toggle=false, Interval=500) {
     try {
-      if(typeof Text === "string") {
+      if(typeof Text === "string") { // Done
         this.btntxt = Text;
       } else { throw 1; }
       if(typeof Key === "number") {
@@ -12,17 +12,51 @@ class Button {
       if(typeof Callback === "function") {
         this.callback = Callback;
       } else { throw 3; }
+      if(typeof Toggle === "boolean") {
+          this.toggle = Toggle;
+      } else { throw 4; }
       if(typeof Interval === "number") {
         this.interval = Interval;
-      } else { throw 4; }
+      } else { throw 5; }
     } 
     catch(e) {
       console.error(e)
     }
       finally {
         // Buttons code here.
-      }
-  }
+        
+        // Goals.. Enable Disable Hack, React to Keytouches, Change css class on touch/click.
+        // Create global array if not created already too hold
+        //     "Keys" So we can throw an error if 2 buttons have the same key set
+        
+        var enabled=false; // If our hack is enabled (For toggles)
+        
+        // Generate ID
+        this.id="h-bar" + Math.floor(Math.random() * 20000);
+        
+        // Check if our ID Matches any others on our hackbar. If so , Regenerate.
+        for(let i=0;i<document.getElementById("btn-ctr").childNodes.length;i++) {
+            if(this.id === document.getElementById("btn-ctr").childNodes[i].id) {
+                this.id="h-bar" + Math.floor(Math.random() * 20000);
+            } // If-con End
+            
+            
+            console.log(document.getElementById("btn-ctr").childNodes[i].id);
+        } // For-loop end
+        
+        // Check if global array is declared
+        if(window.gKeys != undefined || window.gKeys != null) {
+            for(let i=0;i<window.gKeys.length;i++) {
+                if(this.key === window.gKeys[i]) {
+                    console.error(`A Button key is a Duplicate`)
+                }
+            }
+            window.gKeys.push[this.key]
+        }
+      } // Finally End
+  } // constructor end
+  
+  
   
   appendTo($_0) {
      // Target The Menu
@@ -32,18 +66,28 @@ class Button {
       
       // Setting Button Attributes
       $_2.setAttribute("class", "menu-btn");
+      $_2.setAttribute("id", this.id);
       $_2.textContent = this.btntxt;
       
+      // Add Button to our Hackbar
       $_1.appendChild($_2);
   }
 }
 
-
-let t_health = new Button("Healthy", 49, () => {
-    
+// Create an instance of our button.
+let t_health = new Button("Toggle Health", 49, () => {
+    console.log("Health is running!");
 }, 0);
-
-
 t_health.appendTo("btn-ctr");
 
+let t_ammo = new Button("Toggle Ammo", 50, () => {
+    console.log("Ammo is running!");
+}, 0);
+t_ammo.appendTo("btn-ctr");
+
+//console.log(document.getElementById("btn-ctr").childNodes[3].text);
 }
+
+
+
+
